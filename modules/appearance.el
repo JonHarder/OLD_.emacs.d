0929d987ff834e01aa9b18ebd8990bcaa9fdff2a
Jonathan Harder <jonharder6@gmail.com>
2019-02-16 00:21:58 -0600

    revert some doom solarized changes

--- modules/appearance.el
+++ modules/appearance.el
@@ -71,9 +71,6 @@
    ((string-match (rx bol "doom-" (zero-or-more anything)) jh/color-theme)
     (use-package doom-themes
       :ensure t
-      :init
-      (setq doom-solarized-light-brighter-comments t
-	    doom-solarized-light-brighter-modeline t)
       :config
       (load-theme (intern jh/color-theme) t)))
    ((member jh/color-theme '("solarized-light" "solarized-dark"))
