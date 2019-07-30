c47f4d28c706b1cef9bf0281ffc6e76875684005
Jon Harder <jonharder6@gmail.com>
2019-07-29 23:03:33 -0500

    simplify bootstrap

--- bootstrap.el
+++ bootstrap.el
@@ -4,6 +4,8 @@
 ;; initializes package.el, loads use-package, defines the module concept.
 
 ;;; Code:
+
+;; Bootstrap straight.el
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
@@ -16,8 +18,9 @@
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
-
 (straight-use-package 'use-package)
+;; End bootstrap straight.el
+
 
 (defun load-module (module-name &optional module-path)
   "Load the module called MODULE-NAME defined in my modules folder, or in MODULE-PATH if given."
@@ -27,17 +30,12 @@
     (load path)))
 
 
-(defun defmodules (modules)
-  "Load the given MODULES."
-  (mapc 'load-module modules))
-
-
 (defun jh/config-init (config)
   "Initialize configuration using settings found in CONFIG."
   (let ((font (plist-get config :font))
         (font-size (plist-get config :font-size))
         (modules (plist-get config :modules)))
-    (defmodules (cadr modules))
+    (mapc 'load-module (cadr modules))
     (set-frame-font (format "%s %i" font font-size))))
 
 
