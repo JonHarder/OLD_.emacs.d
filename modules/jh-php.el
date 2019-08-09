bb3ee3340d66ee5458d0772e03dc60dcc29595df
Jonathan Harder <jonharder6@gmail.com>
2019-08-09 11:28:58 -0500

    intelisense for php

--- modules/jh-php.el
+++ modules/jh-php.el
@@ -44,6 +44,39 @@
 (use-package phpunit
   :straight t)
 
+(use-package lsp-mode
+  :straight t
+  :hook (php-mode . lsp)
+  :commands lsp
+  :config
+  (setq lsp-prefer-flymake nil))
+  
+
+(use-package lsp-mode
+  :straight t
+  :config
+  (setq lsp-prefer-flymake nil)
+  :hook (php-mode . lsp)
+  :commands lsp)
+
+(use-package lsp-ui
+  :straight t
+  :requires lsp-mode flycheck
+  :config
+  (setq lsp-ui-doc-enable t
+        lsp-ui-doc-use-childframe t
+        lsp-ui-doc-position 'top
+        lsp-ui-doc-include-signature t
+        lsp-ui-sideline-enable nil
+        lsp-ui-flycheck-enable t
+        lsp-ui-flycheck-list-position 'right
+        lsp-ui-flycheck-live-reporting t
+        lsp-ui-peek-enable t
+        lsp-ui-peek-list-width 60
+        lsp-ui-peek-peek-height 25
+        lsp-ui-sideline-enable nil)
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+
 
 (provide 'jh-php)
 ;;; jh-php.el ends here
