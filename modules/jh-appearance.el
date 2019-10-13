e0b188e92e24b711d6b9ea4950857ab3077c3b17
Jon Harder <jonharder6@gmail.com>
2019-10-13 08:53:12 -0500

    toggle highlight lines mode in config

--- modules/jh-appearance.el
+++ modules/jh-appearance.el
@@ -30,6 +30,11 @@
   (show-paren-mode 1)
   (electric-pair-mode 1)
   (global-prettify-symbols-mode +1)
+  (menu-bar-mode -1)
+  (toggle-scroll-bar -1)
+  (tool-bar-mode -1)
+  (when (alist-get :highlight-line config nil)
+    (add-hook 'prog-mode-hook 'hl-line-mode))
 
   (use-package rainbow-delimiters
     :config
@@ -39,11 +44,6 @@
         (color-theme-package (alist-get :color-theme-package config))
         (font (alist-get :font config))
         (font-size (alist-get :font-size config)))
-    (menu-bar-mode -1)
-    (toggle-scroll-bar -1)
-    (tool-bar-mode -1)
-    (add-hook 'prog-mode-hook 'hl-line-mode)
-
     (jh/set-theme color-theme color-theme-package)
     (set-frame-font (format "%s %s" font font-size))))
 
