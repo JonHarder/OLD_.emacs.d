de886fc21fee95f5487d9766f8f73d615bfd2e06
Jonathan Harder <jonharder6@gmail.com>
2019-12-23 16:33:01 -0600

    parinfer

--- modules/jh-lisp.el
+++ modules/jh-lisp.el
@@ -9,7 +9,21 @@
     :straight t
     :init
     (setq inferior-lisp-program "/usr/bin/sbcl"
-          slime-contribs '(slime-fancy))))
+          slime-contribs '(slime-fancy)))
+
+  (use-package parinfer
+    :init
+    (progn
+      (setq parinfer-extensions
+            '(defaults
+              pretty-parens
+              evil
+              smart-tab))
+      (add-hook 'clojure-mode-hook #'parinfer-mode)
+      (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
+      (add-hook 'common-lisp-mode-hook #'parinfer-mode)
+      (add-hook 'scheme-mode-hook #'parinfer-mode)
+      (add-hook 'lisp-mode-hook #'parinfer-mode))))
 
 
 (provide 'jh-clojure)
