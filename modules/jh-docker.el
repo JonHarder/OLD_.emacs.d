3ace68d208737655b194bd3e68fc9c4506e6d61d
Jonathan Harder <jonharder6@gmail.com>
2020-01-29 13:58:22 -0600

    use regular emacs mode when in docker commands

--- modules/jh-docker.el
+++ modules/jh-docker.el
@@ -6,9 +6,18 @@
 ;;; Code:
 (defun modules/docker--load (config)
   "Use CONFIG to load Dockerfile syntax and an interactive docker UI."
+
   (use-package dockerfile-mode
     :mode "\\.Dockerfile")
-  (straight-use-package 'docker))
+
+  (use-package 'docker
+    :config
+    (dolist (mode '(docker-image-mode
+                    docker-container-mode
+                    docker-network-mode
+                    docker-volume-mode))
+      (add-to-list 'evil-emacs-state-modes mode))))
+
 
 (provide 'jh-docker)
 ;;; jh-docker.el ends here
