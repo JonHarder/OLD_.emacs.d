d62b58de57ba71542ace8e1a349bc47c5d5667d0
Jonathan Harder <jonharder6@gmail.com>
2020-02-20 12:11:55 -0600

    include kubernetes support

---
+++ modules/jh-kubernetes.el
@@ -0,0 +1,11 @@
+;;; kubernetes --- Summary
+
+;;; Commentary:
+
+;;; Code:
+
+(straight-use-package 'kubernetes)
+(straight-use-package 'kubernetes-evil)
+
+(provide 'jh-kubernetes)
+;;; jh-kubernetes.el ends here
