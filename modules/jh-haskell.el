84722898dca27c7857846fa14f9fbf5cd8f27922
Jonathan Harder <jonharder6@gmail.com>
2020-06-23 08:46:15 -0500

    enable better haskell interactive integration

--- modules/jh-haskell.el
+++ modules/jh-haskell.el
@@ -6,7 +6,14 @@
 ;;; Code:
 (defun modules/haskell--load (config)
   "Load configuration for Haskell using CONFIG."
-  (straight-use-package 'haskell-mode))
+  (straight-use-package 'haskell-mode)
+  (require 'haskell-interactive-mode)
+  (require 'haskell-process)
+  (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+  (custom-set-variables
+   '(haskell-process-suggest-remove-import-lines t)
+   '(haskell-process-auto-import-loaded-modules t)
+   '(haskell-process-log t)))
   ;; (use-package lsp-haskell
   ;;   :demand
   ;;   :config
