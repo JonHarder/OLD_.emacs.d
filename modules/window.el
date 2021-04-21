7f28b8f55e2b4649139e52caa5c5f8414010b762
Jonathan Harder <jonharder6@gmail.com>
2021-04-21 10:13:43 -0500

    make vter a side window too

--- modules/window.el
+++ modules/window.el
@@ -1,25 +1,13 @@
 (defun modules/window--load (config)
   "Rules for window placement."
   (setq display-buffer-alist
-        '(("\\*e?shell\\*"
+        '(("\\*\\(e?shell\\|vterm\\)\\*"
            (display-buffer-in-side-window)
            (window-height . 0.25)
            (side . bottom)
            (slot . -1))
-          ("\\*\\(Backtrace\\|Warnings\\|Compile-Log\\|helpful .*: .*\\|Messages\\)\\*"
+          ("\\*\\(Backtrace\\|Warnings\\|Compile-Log\\|helpful .*: .*\\|Help\\|Messages\\)\\*"
            (display-buffer-in-side-window)
            (window-height . 0.25)
            (side . bottom)
-           (slot . 0))
-          ("\\*Faces\\*"
-           (display-buffer-in-side-window)
-           (side . bottom)
-           (slot . 1))
-          ("Dired by name"
-           (display-buffer-in-side-window)
-           (slot . 0)
-           (side . left)
-           (window-width . 0.2)
-           (window-parameters . ((no-other-window . t)
-                                 (no-delete-other-windows . t)
-                                 (mode-line-format . (" " "%b"))))))))
+           (slot . 0)))))
